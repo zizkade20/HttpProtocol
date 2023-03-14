108f9560813cb17031e2afe357c199f51fb218bd
zizka <zizkade20@sps-prosek.cz>
2023-03-14 10:02:08 +0100

    bramborovy salat

--- HttpProtocol/Class1.cs
+++ HttpProtocol/Class1.cs
@@ -1,18 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
 
-namespace HttpProtocol
+public class URLChecker
 {
-    public class HttpCheck
+    public void Run()
     {
+        Console.WriteLine("Napiš adresu, kterou chceš ověřit:");
+        string url = Console.ReadLine();
+        bool a = true;
+        while (a)
+        {
+            try
+            {
+                
 
-
-
-
-
+                if (IsValidUrl(url))
+                {
+                    HttpWebResponse response = SendRequest(url);
+                    int integerStatus = (int)response.StatusCode;
+                    string stringStatus = response.StatusDescription;
+                    Console.WriteLine(integerStatus + " (" + stringStatus + ")");
+                }
+                else
+                {
+                    Console.WriteLine("Input neni validni");
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Thread.Sleep(1000);
+        }
     }
+
+    private bool IsValidUrl(string url)
+    {
+        var regex = new Regex("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
+        return regex.IsMatch(url);
+    }
+
+    private HttpWebResponse SendRequest(string url)
+    {
+        WebRequest request = WebRequest.Create(url);
+        return (HttpWebResponse)request.GetResponse();
+    }
+
 }
--- HttpProtocol/Program.cs
+++ HttpProtocol/Program.cs
@@ -12,47 +12,8 @@ namespace HttBS
     {
         static void Main()
         {
-            bool a = true;
-            while (a)
-            {
-                try
-                {
-                    Console.WriteLine("Napiš adresu, kterou chceš ověřit:");
-
-                    string url = Console.ReadLine();
-
-                    var regex = new Regex("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
-
-                    
-
-                    if (regex.IsMatch(url))
-                    {
-
-                        WebRequest request = WebRequest.Create(url);
-
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                        int integerStatus = (int)response.StatusCode;
-                        string stringStatus = response.StatusDescription;
-
-                        Console.WriteLine(integerStatus + " (" + stringStatus + ")");
-                    } else
-                    {
-                        Console.WriteLine("Input neni validni");
-                    }
-
-
-                }
-                catch (WebException e)
-                {
-                    //HttpWebResponse response = (HttpWebResponse)e.Response;
-                    Console.WriteLine(e.Message);
-                }
-                Thread.Sleep(1000);
-            }
-            
-
-
+            URLChecker urlChecker = new URLChecker();
+            urlChecker.Run();
         }
     }
     
